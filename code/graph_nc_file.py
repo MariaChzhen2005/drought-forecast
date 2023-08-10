35446fd4157cbd3668bfa7eadcf6e44f2e6ea4a3
MariaChzhen2005 <46852765+MariaChzhen2005@users.noreply.github.com>
2023-08-10 13:57:51 -0400

    Create graph_nc_file.py
    Graph netcdf file:
    - top 5% of data removed
    - logarithm of the data
    - original data

---
+++ code/graph_nc_file.py
@@ -0,0 +1,38 @@
+import xarray as xr
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+file_path = '/Users/mariachzhen/Desktop/SF2023/Data/agg-120m-runoff-ERA5.nc'
+file_new = xr.open_dataset(file_path)
+file_new = file_new.to_array()
+print("file_new converted to array")
+file_new = file_new.values
+print("file converted to numpy ndarray")
+
+data = file_new[0,0,:,:]
+plotted = np.nanpercentile(data, 95)
+
+masked_data = np.where(data > plotted, np.nan, data)
+log_data = np.log(data)
+
+plt.imshow(masked_data, cmap='cool')
+plt.colorbar(label='Runoff')
+plt.xlabel('Longitude')
+plt.ylabel('Latitude')
+plt.title('Runoff - Aggregated over 120 months - Top 5% data removed')
+plt.show()
+
+plt.imshow(log_data, cmap='cool')
+plt.colorbar(label='Runoff')
+plt.xlabel('Longitude')
+plt.ylabel('Latitude')
+plt.title('Runoff - Aggregated over 120 months - Logarithm of the data')
+plt.show()
+
+plt.imshow(data, cmap='cool')
+plt.colorbar(label='Runoff')
+plt.xlabel('Longitude')
+plt.ylabel('Latitude')
+plt.title('Runoff - Aggregated over 120 months - Original')
+plt.show()
